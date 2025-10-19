936d661756076a9040af66b5b605c230c64f176e
Roman <kowaliszyn@gazeta.pl>
2025-10-19 13:58:37 +0200

    Correct a comment

--- src/LinearRegression/Program.cs
+++ src/LinearRegression/Program.cs
@@ -64,7 +64,7 @@ for (int iteration = 1; iteration <= Iterations; iteration++)
         Console.WriteLine($"Iteration: {iteration,5}, MSE: {meanSquaredError,10:F5}, ∂MSE/∂a: {deltaA,10:F4}, ∂MSE/∂b: {deltaB,10:F4}, a: {a,9:F4}, b: {b,9:F4}");
 }
 
-// 4. Output learned parameters
+// 5. Output learned parameters
 
 Console.WriteLine();
 Console.WriteLine($"Learned parameters:");
