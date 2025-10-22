bd560a8e49c1273df881c60f4519421840c5868d
Roman <kowaliszyn@gazeta.pl>
2025-10-22 12:51:19 +0200

    New project: MultipleLinearRegression

---
+++ src/MultipleLinearRegression/Program.cs
@@ -0,0 +1,33 @@
+﻿// Neural Networks in C♯
+// File name: Program.cs
+// www.kowaliszyn.pl, 2025
+
+Console.WriteLine("Select a routine to run:");
+Console.WriteLine("1. Variables");
+Console.WriteLine("2. Tables");
+Console.WriteLine("3. Matrices");
+Console.WriteLine("4. Matrices with bias");
+// exit
+Console.WriteLine("Other: Exit");
+Console.Write("Enter your choice: ");
+
+string? choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        // Variables();
+        break;
+    case "2":
+        // Tables();
+        break;
+    case "3":
+        // Matrices();
+        break;
+    case "4":
+        // MatricesWithBias();
+        break;
+    default:
+        Console.WriteLine("Goodbye!");
+        break;
+}
