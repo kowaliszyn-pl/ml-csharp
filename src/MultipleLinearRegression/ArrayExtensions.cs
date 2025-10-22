d06dca41ce46a35bf88885e064dc30cf5e722e92
Roman <kowaliszyn@gazeta.pl>
2025-10-22 17:58:09 +0200

    Move ArrayExtensions to another file

---
+++ src/MultipleLinearRegression/ArrayExtensions.cs
@@ -0,0 +1,146 @@
+﻿// Neural Networks in C♯
+// File name: ArrayExtensions.cs
+// www.kowaliszyn.pl, 2025
+
+using System.Diagnostics;
+
+public static class ArrayExtensions
+{
+    public static float[,] MultiplyDot(this float[,] source, float[,] matrix)
+    {
+
+        Debug.Assert(source.GetLength(1) == matrix.GetLength(0));
+
+        int matrixColumns = matrix.GetLength(1);
+
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+
+        float[,] res = new float[rows, matrixColumns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < matrixColumns; j++)
+            {
+                float sum = 0;
+                for (int k = 0; k < columns; k++)
+                {
+                    sum += source[i, k] * matrix[k, j];
+                }
+                res[i, j] = sum;
+            }
+        }
+
+        return res;
+    }
+
+    public static float[,] Add(this float[,] source, float scalar)
+    {
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+        float[,] res = new float[rows, columns];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                res[row, col] = source[row, col] + scalar;
+            }
+        }
+
+        return res;
+    }
+
+    public static float[,] Subtract(this float[,] source, float[,] matrix)
+    {
+        Debug.Assert(source.GetLength(0) == matrix.GetLength(0));
+        Debug.Assert(source.GetLength(1) == matrix.GetLength(1));
+
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+        float[,] res = new float[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                res[i, j] = source[i, j] - matrix[i, j];
+            }
+        }
+
+        return res;
+    }
+
+    public static float[,] Power(this float[,] source, int scalar)
+    {
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+        float[,] res = new float[rows, columns];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                res[row, col] = MathF.Pow(source[row, col], scalar);
+            }
+        }
+
+        return res;
+    }
+
+    public static float Mean(this float[,] source)
+        => source.Sum() / source.Length;
+
+    public static float Sum(this float[,] source)
+    {
+        // Sum over all elements.
+        float sum = 0;
+        int rows = source.GetLength(0);
+        int cols = source.GetLength(1);
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                sum += source[row, col];
+            }
+        }
+
+        return sum;
+    }
+
+    public static float[,] Transpose(this float[,] source)
+    {
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+
+        float[,] array = new float[columns, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                array[j, i] = source[i, j];
+            }
+        }
+
+        return array;
+    }
+
+    public static float[,] Multiply(this float[,] source, float scalar)
+    {
+        int rows = source.GetLength(0);
+        int columns = source.GetLength(1);
+        float[,] res = new float[rows, columns];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                res[row, col] = source[row, col] * scalar;
+            }
+        }
+
+        return res;
+    }
+}
--- src/MultipleLinearRegression/Program.cs
+++ src/MultipleLinearRegression/Program.cs
@@ -4,8 +4,6 @@
 
 // Set the parameters for the model
 
-using System.Diagnostics;
-
 const float LearningRate = 0.0005f;
 const int Iterations = 35_000;
 const int PrintEvery = 1_000;
@@ -87,7 +85,7 @@ void Variables()
         float sumErrorForA1 = 0;
         float sumErrorForA2 = 0;
         float sumErrorForA3 = 0;
-        float sumErrorForB = 0;  
+        float sumErrorForB = 0;
 
         foreach (float[] sample in data)
         {
@@ -282,154 +280,14 @@ void Matrices()
 
         if (iteration % PrintEvery == 0)
         {
-            Console.WriteLine($"Iteration: {iteration,6} | MSE: {meanSquaredError,8:F5} | a1: {A[0,0],8:F4} | a2: {A[1,0],8:F4} | a3: {A[2,0],8:F4} | b: {b,8:F4}");
+            Console.WriteLine($"Iteration: {iteration,6} | MSE: {meanSquaredError,8:F5} | a1: {A[0, 0],8:F4} | a2: {A[1, 0],8:F4} | a3: {A[2, 0],8:F4} | b: {b,8:F4}");
         }
     }
 
     // 4. Output learned parameters
 
     Console.WriteLine("\n--- Training Complete (Matrices) ---");
-    Console.WriteLine($"{"Learned parameters:",-20} a1 = {A[0,0],9:F4} | a2 = {A[1,0],9:F4} | a3 = {A[2,0],9:F4} | b = {b,9:F4}");
+    Console.WriteLine($"{"Learned parameters:",-20} a1 = {A[0, 0],9:F4} | a2 = {A[1, 0],9:F4} | a3 = {A[2, 0],9:F4} | b = {b,9:F4}");
     Console.WriteLine($"{"Expected parameters:",-20} a1 = {2,9:F4} | a2 = {3,9:F4} | a3 = {-1,9:F4} | b = {5,9:F4}");
 }
 
-public static class ArrayExtensions
-{
-    public static float[,] MultiplyDot(this float[,] source, float[,] matrix)
-    {
-
-        Debug.Assert(source.GetLength(1) == matrix.GetLength(0));
-
-        int matrixColumns = matrix.GetLength(1);
-
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-
-        float[,] res = new float[rows, matrixColumns];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < matrixColumns; j++)
-            {
-                float sum = 0;
-                for (int k = 0; k < columns; k++)
-                {
-                    sum += source[i, k] * matrix[k, j];
-                }
-                res[i, j] = sum;
-            }
-        }
-
-        return res;
-    }
-
-    public static float[,] Add(this float[,] source, float scalar)
-    {
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-        float[,] res = new float[rows, columns];
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < columns; col++)
-            {
-                res[row, col] = source[row, col] + scalar;
-            }
-        }
-
-        return res;
-    }
-
-    public static float[,] Subtract(this float[,] source, float[,] matrix)
-    {
-        Debug.Assert(source.GetLength(0) == matrix.GetLength(0));
-        Debug.Assert(source.GetLength(1) == matrix.GetLength(1));
-
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-        float[,] res = new float[rows, columns];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                res[i, j] = source[i, j] - matrix[i, j];
-            }
-        }
-
-        return res;
-    }
-
-    public static float[,] Power(this float[,] source, int scalar)
-    {
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-        float[,] res = new float[rows, columns];
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < columns; col++)
-            {
-                res[row, col] = MathF.Pow(source[row, col], scalar);
-            }
-        }
-
-        return res;
-    }
-
-    public static float Mean(this float[,] source)
-        => source.Sum() / source.Length;
-
-    public static float Sum(this float[,] source)
-    {
-        // Sum over all elements.
-        float sum = 0;
-        int rows = source.GetLength(0);
-        int cols = source.GetLength(1);
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols; col++)
-            {
-                sum += source[row, col];
-            }
-        }
-
-        return sum;
-    }
-
-    public static float[,] Transpose(this float[,] source)
-    {
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-
-        float[,] array = new float[columns, rows];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                array[j, i] = source[i, j];
-            }
-        }
-
-        return array;
-    }
-
-    public static float[,] Multiply(this float[,] source, float scalar)
-    {
-        int rows = source.GetLength(0);
-        int columns = source.GetLength(1);
-        float[,] res = new float[rows, columns];
-
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < columns; col++)
-            {
-                res[row, col] = source[row, col] * scalar;
-            }
-        }
-
-        return res;
-    }
-}
