a87a5d4b8b67593a0156b2a7e1de38c6dc7d42b5
Roman <kowaliszyn@gazeta.pl>
2025-12-24 15:35:34 +0100

    InvertedDropout2D

---
+++ src/NeuralNetworks/Operations/InvertedDropout2D.cs
@@ -0,0 +1,41 @@
+﻿// Neural Networks in C♯
+// File name: InvertedDropout2D.cs
+// www.kowaliszyn.pl, 2025
+
+using System.Diagnostics;
+
+using NeuralNetworks.Core;
+using NeuralNetworks.Operations.Interfaces;
+
+namespace NeuralNetworks.Operations;
+
+public class InvertedDropout2D(float keepProb = 0.8f, SeededRandom? random = null) : Operation2D, IParameterCountProvider
+{
+    private float[,]? _mask;
+    readonly float _multiplier = 1f / keepProb;
+
+    protected override float[,] CalcOutput(bool inference)
+    {
+        if (inference)
+        {
+            return Input;
+        }
+        else
+        {
+            _mask = Input.AsZeroOnes(keepProb, random ?? new());
+            return Input.MultiplyElementwise(_mask).Multiply(_multiplier);
+        }
+    }
+
+    protected override float[,] CalcInputGradient(float[,] outputGradient)
+    {
+        Debug.Assert(_mask != null, "Mask must not be null here.");
+        return outputGradient.MultiplyElementwise(_mask).Multiply(_multiplier);
+    }
+
+    public override string ToString() => $"InvertedDropout2D (keepProb={keepProb}, seed={random?.Seed})";
+
+    public int GetParamCount()
+        => _mask?.Length ?? 0;
+
+}
