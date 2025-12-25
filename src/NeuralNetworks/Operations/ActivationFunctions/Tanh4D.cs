48b924a53ddd8624b3c387ac49f719adc3005863
Roman <kowaliszyn@gazeta.pl>
2025-12-25 13:10:15 +0100

    Documentation comments

--- src/NeuralNetworks/Operations/ActivationFunctions/Tanh4D.cs
+++ src/NeuralNetworks/Operations/ActivationFunctions/Tanh4D.cs
@@ -11,17 +11,35 @@ public class Tanh4D : ActivationFunction4D
     protected override float[,,,] CalcOutput(bool inference)
         => Input.Tanh();
 
-    protected override float[,,,] CalcInputGradient(float[,,,] outputGradient) =>
-        // The CalcInputGradient function computes the gradient of the loss with respect to the input of the Tanh function.
-        // This is done using the chain rule of calculus. Given the output gradient (dL/dy), the function calculates the input gradient (dL/dx).
-        // The derivative of the Tanh function tanh(x) = (exp(x) - exp(-x)) / (exp(x) + exp(-x)) is 1 - tanh(x)^2.
-        // Therefore, the input gradient is computed as: dL/dx = dL/dy * (1 - tanh(x)^2).
-        // The elementwise multiplication of the output gradient and the derivative of the Tanh function is returned as the input gradient.
-        // tanh(x) => Output
-        // dL/dy => outputGradient
-        // dl/dx => inputGradient
-
-        outputGradient.MultiplyByTanhDerivativeSpan(Output);
+    /// <summary>
+    /// Calculates the gradient of the loss with respect to the input of the Tanh activation function.
+    /// </summary>
+    /// <remarks>
+    /// This method is typically used during backpropagation in neural network training to propagate
+    /// gradients through a Tanh activation layer. The returned array has the same shape as <paramref
+    /// name="outputGradient"/>.
+    /// <para>
+    /// This is done using the chain rule of calculus. Given the output gradient (dL/dy), the function calculates the input gradient (dL/dx). The derivative of the Tanh function is <c>1 - tanh(x)^2</c>. Therefore, the input gradient is computed as: <c>dL/dx = dL/dy * (1 - tanh(x)^2)</c>. The elementwise multiplication of the output gradient and the derivative of the Tanh function is returned as the input gradient.
+    /// </para>
+    /// <list type="bullet">
+    /// <item>
+    /// tanh(x) => Output
+    /// </item>
+    /// <item>
+    /// dL/dy => outputGradient
+    /// </item>
+    /// <item>
+    /// dl/dx => inputGradient
+    /// </item>
+    /// </list>
+    /// </remarks>
+    /// <param name="outputGradient">A four-dimensional array representing the gradient of the loss with respect to the output of the Tanh function. The shape must match the output tensor of the layer.</param>
+    /// <returns>A four-dimensional array containing the gradient of the loss with respect to the input of the Tanh function.
+    /// Each element is computed by multiplying the corresponding element in <paramref name="outputGradient"/> by the
+    /// derivative of the Tanh function at that position.
+    /// </returns>
+    protected override float[,,,] CalcInputGradient(float[,,,] outputGradient) 
+        => outputGradient.MultiplyByTanhDerivativeSpan(Output);
 
     public override string ToString() => "Tanh4D";
 }
