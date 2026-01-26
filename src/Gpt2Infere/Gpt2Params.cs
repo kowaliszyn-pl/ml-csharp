c23a0c7d59c06de6ecfd5890a03af1aea45685f2
Roman <kowaliszyn@gazeta.pl>
2026-01-26 11:55:05 +0100

    Use init-only properties for immutability

--- src/Gpt2Infere/Gpt2Params.cs
+++ src/Gpt2Infere/Gpt2Params.cs
@@ -7,36 +7,36 @@
 
 internal sealed record Gpt2Params
 {
-    public float[,] TokenEmbeddings { get; set; } = default!;
-    public float[,] PositionalEmbeddings { get; set; } = default!;
-    public Gpt2Block[] Blocks { get; set; } = default!;
-    public Gpt2LayerNormParams FinalLayerNorm { get; set; } = default!;
+    public float[,] TokenEmbeddings { get; init; } = default!;
+    public float[,] PositionalEmbeddings { get; init; } = default!;
+    public Gpt2Block[] Blocks { get; init; } = default!;
+    public Gpt2LayerNormParams FinalLayerNorm { get; init; } = default!;
 }
 
 internal sealed record Gpt2Block
 {
-    public Gpt2LayerNormParams LayerNorm1 { get; set; } = default!;
-    public Gpt2LayerNormParams LayerNorm2 { get; set; } = default!;
-    public Gpt2MultiHeadAttentionParams Attention { get; set; } = default!;
-    public Gpt2MultiLayerPerceptron MultiLayerPerceptron { get; set; } = default!;
+    public Gpt2LayerNormParams LayerNorm1 { get; init; } = default!;
+    public Gpt2MultiHeadAttentionParams Attention { get; init; } = default!;
+    public Gpt2LayerNormParams LayerNorm2 { get; init; } = default!;
+    public Gpt2MultiLayerPerceptron MultiLayerPerceptron { get; init; } = default!;
 }
 
 internal sealed record Gpt2LayerNormParams
 {
-    public float[] Gamma { get; set; } = default!;
-    public float[] Beta { get; set; } = default!;
+    public float[] Gamma { get; init; } = default!;
+    public float[] Beta { get; init; } = default!;
 }
 
 internal sealed record Gpt2MultiHeadAttentionParams
 {
-    public Gpt2LinearParams Projection { get; set; } = default!;
-    public Gpt2LinearParams OutputProjection { get; set; } = default!;
+    public Gpt2LinearParams Projection { get; init; } = default!;
+    public Gpt2LinearParams OutputProjection { get; init; } = default!;
 }
 
 internal sealed record Gpt2LinearParams
 {
-    public float[,] Weights { get; set; } = default!;
-    public float[] Bias { get; set; } = default!;
+    public float[,] Weights { get; init; } = default!;
+    public float[] Bias { get; init; } = default!;
 }
 
 internal sealed record Gpt2MultiLayerPerceptron
@@ -44,10 +44,10 @@ internal sealed record Gpt2MultiLayerPerceptron
     /// <summary>
     /// Multi layer perceptron, full connected
     /// </summary>
-    public Gpt2LinearParams FullyConnected { get; set; } = default!;
+    public Gpt2LinearParams FullyConnected { get; init; } = default!;
 
     /// <summary>
     /// Multi layer perceptron, projection
     /// </summary>
-    public Gpt2LinearParams OutputProjection { get; set; } = default!;
+    public Gpt2LinearParams OutputProjection { get; init; } = default!;
 }
