32aefd2d0825c83ad5d42b6bd36f7611bdee5bbb
Roman <kowaliszyn@gazeta.pl>
2026-04-26 18:28:05 +0200

    Add some comments

--- src/NeuralNetworks/Losses/MeanSquaredErrorLoss.cs
+++ src/NeuralNetworks/Losses/MeanSquaredErrorLoss.cs
@@ -8,6 +8,12 @@ using static NeuralNetworks.Core.Operations.OperationBackend;
 
 namespace NeuralNetworks.Losses;
 
+/// <summary>
+/// Represents a loss function that computes the mean squared error between predicted and target values.
+/// </summary>
+/// <remarks>This class is typically used in regression tasks (for example, Boston Housing dataset, Sine wave prediction) to measure the average squared difference between
+/// predicted and actual values. It can be used as a loss function in training neural networks or other predictive
+/// models where minimizing the mean squared error is desired.</remarks>
 public class MeanSquaredErrorLoss : Loss<float[,]>
 {
     private float[,]? _errors;
--- src/NeuralNetworks/Losses/MeanSquaredErrorLoss4D.cs
+++ src/NeuralNetworks/Losses/MeanSquaredErrorLoss4D.cs
@@ -8,6 +8,9 @@ using static NeuralNetworks.Core.Operations.OperationBackend;
 
 namespace NeuralNetworks.Losses;
 
+/// <remarks>
+/// Usually used in convolutional neural networks (CNNs) for autoencoding tasks, where the input and output are 4D tensors (e.g., batch size, channels, height, width).
+/// </remarks>
 public class MeanSquaredErrorLoss4D : Loss<float[,,,]>
 {
     private float[,,,]? _errors;
